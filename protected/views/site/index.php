fb7c3d0a19195abf2b8a360486c5b462afa2de75
Anuar <jaicof@gmail.com>
2014-06-20 16:16:40 -0600

    fixed table alignment.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -161,7 +161,7 @@ $this->pageTitle=Yii::app()->name;
 							<h4><?php echo $user['YTD2']; ?></h4>
 						</div>
 					</div>
-				<!-- </div> -->
+				</div>
 			</div>
 
 		<?php endforeach ?>
@@ -207,33 +207,6 @@ $this->pageTitle=Yii::app()->name;
 <script src='js/sort.js'></script>
 <script type="text/javascript" charset="utf-8">
 $(function() {
-	// jQuery('.mdk_toggable_list').each(function() {
- //        var divFirst = jQuery(this).find('div:first');
- //        divFirstHtml = divFirst.html();
- //        if (jQuery(this).hasClass('mdk_toggable_initial_show')) {
- //            divFirst.html('<div class=""><div style="float:left"><a href="#" onClick="mdk_togglable_link(jQuery(this),event);" class="mdk_toggable_showing mdk_toggable_link"><img src="/img/layout_default/ico_toggle_show.png" style="width:15px;margin-right:0.5em" /></a></div><div>' + divFirstHtml + '</div></div>');
- //        } else {
- //            divFirst.html('<div class="row"><div style="float:left"><a href="#" onClick="mdk_togglable_link(jQuery(this),event);" class="mdk_toggable_hidden mdk_toggable_link"><img src="/img/layout_default/ico_toggle_hidden.png" style="width:15px;margin-right:0.5em" /></a></div><div>' + divFirstHtml + '</div></div>');
- //        }
- //    });
 
-	function togglable_link(object, e) {
-	    e.preventDefault();
-	    if (object.hasClass('toggable_hidden')) {
-	        
-	        object.removeClass('mdk_toggable_hidden');
-	        object.addClass('toggable_showing');
-	        var parent = object.closest('.mdk_toggable_list');
-	        parent.find('.mdk_toggable_list_details:first').show(function() {
-	            jQuery.event.trigger('mdk_toggable_open', [object]);
-	        });
-	    } else {
-	        jQuery(object).find('img:first').attr('src', '/img/layout_default/ico_toggle_hidden.png');
-	        object.removeClass('mdk_toggable_showing');
-	        object.addClass('mdk_toggable_hidden');
-	        var parent = object.closest('.mdk_toggable_list');
-	        parent.find('.mdk_toggable_list_details:first').hide();
-	    }
-	}
 });
 </script>
