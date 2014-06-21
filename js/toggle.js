296ac58cd86785eda809861d5788daa846d58e11
Anuar <jaicof@gmail.com>
2014-06-20 19:58:54 -0600

    sorted works on child tables.

--- js/toggle.js
+++ js/toggle.js
@@ -1,11 +1,11 @@
 $(function() {
-	$('.second-row, .third-row').hide();
+	$('.second-row, .third-row, .total-second').hide();
 	// toggle animation when clicking YTD values.
-	$(".sort p.ytd").click(function() {
+	$(".data p.ytd").click(function() {
 		$(this).parent()
 			.parent()
 			.parent()
-			.find('.second-row')
+			.find('.second-row, .total-second')
 			.slideToggle(300);
 	});
 	$('.second-row .detail-prod').click(function() {
